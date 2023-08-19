87e11e648f8d402eb3b0120dbf5fbc21f17edb02
christinecheng-cs <53107285+christinecheng-cs@users.noreply.github.com>
2023-08-18 23:09:32 -0700

    Create 10001st_prime.py

---
+++ 10001st_prime.py
@@ -0,0 +1,19 @@
+'''
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13. 
+
+What is the 10 001st prime number? 
+
+Source: Project Euler
+'''
+def primeGenerator():
+    while (num <= 10001):
+      if isPrime(i):
+        num = num + 1
+      i = i + 2
+    return 0
+
+def isPrime(num):
+  return False
+
+
+
