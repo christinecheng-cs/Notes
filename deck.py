73e828b898c36d0c94d1204a54e51b69ac0e99eb
christinecheng-cs <53107285+christinecheng-cs@users.noreply.github.com>
2023-10-29 22:40:07 -0700

    Create deck.py

--- deck.py
+++ deck.py
@@ -1,1 +1,19 @@
+import collections
+
+
+Cards = collections.namedtuple('Cards', ['rank', 'suit'])
+
+class Deck:
+  ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+  suits = 'spades diamonds clubs hearts'.split()
+
+  def __init__(self):
+    self._cards = [Card(rank, suit) for suit in self.suits for rank in self.ranks]
+                   
+  def __getitem__(self, position):
+    return self._cards[position]
+    
+  def __len__(self):
+    pass
+
 
